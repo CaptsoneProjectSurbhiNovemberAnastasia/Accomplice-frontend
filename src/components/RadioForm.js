0ee9ade755796cf8d0ee81c76a1ff4822dea57d9
Anastasia Garelli <anastasia.garelli@gmail.com>
2018-08-16 14:55:41 -0400

    Pulling from master quick

--- src/components/RadioForm.js
+++ src/components/RadioForm.js
@@ -4,18 +4,18 @@ const RadioForm = (props) => {
   const { values, question, handleSubmit, handleChange, handleClick, answer} = props
 
   return (
-    <form >
+    <form  onSubmit={handleSubmit}>
       <div>{question.question}</div><br/>
       {
         values.map(value =>
           <input type='radio' key={value} value={value}  onChange={handleChange} checked={+answer === value}/>
         )
       }
-      <div>Disagree Neutral Agree</div>
+      <div className="scale">Disagree Neutral Agree</div>
       {
         (question.id === 15) ?
-        <button type='submit' onClick={handleClick} onSubmit={handleSubmit}>Submit</button> :
-        <button type='submit' onClick={handleClick}>Next</button>
+        <button type='submit' onClick={handleClick}>Submit</button> :
+        <button type='button' onClick={handleClick}>Next</button>
       }
     </form>
   )
