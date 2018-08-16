18c25de75e811cd820629eecbead5747baf8b24a
Surbhi Gupta <surabhi.gupta.sai@gmail.com>
2018-08-15 22:38:23 -0400

    Swipe is working

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -9,12 +9,21 @@ class UserCard extends Component {
   }
   render() {
     const { user } = this.props
+    // console.log('User is', user)
     return (
-      <li>
-        <h4>{user.firstName + ' ' + user.lastName}</h4>
-        <p>{user.description}</p>
-        <button onClick={this.handleClick}>Match!</button>
-      </li>
+      <div id="userCard" className="collapsed">
+        <div>
+          <div id="picContainer">
+            <img className="userPic rounded" src={user.imageUrl} />
+          </div>
+          <li>
+            <h4>{user.firstName + ' ' + user.lastName}</h4>
+            <p>{user.description}</p>
+
+            <button onClick={this.handleClick}>Match!</button>
+          </li>
+        </div>
+      </div>
     )
   }
 }
